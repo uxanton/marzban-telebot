f52d5b9a5ff7690f79517f4dd39a14531668579b
azabolotskikh <a@zblt.ru>
2024-01-04 23:24:41 +0300

    Фикс ошибок

--- telegram/handlers/admin.py
+++ telegram/handlers/admin.py
@@ -515,8 +515,8 @@ def get_template_info_text(
 
 <b>Трафик:</b>: {readable_size(data_limit) if data_limit else 'Безлимит'}
 <b>Дата окончания</b>: {(datetime.now() + relativedelta(seconds=expire_duration)).strftime('%Y-%m-%d') if expire_duration else 'Бесконечно'}
-{'<b>Префикс:</b>' username_prefix if username_prefix}
-{'<b>Суффикс:</b>' username_suffix if username_suffix}
+<b>Префикс:</b> {username_prefix if username_prefix else 'Без префикса'}
+<b>Суффикс:</b> {username_suffix if username_suffix else 'Без суффикса'}
 <b>Протоколы</b>: {protocols}
         """
     return text
